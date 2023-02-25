4654a5391116344d0851094bc64b08d802ae421d
ktmurray1999 <43282256+ktmurray1999@users.noreply.github.com>
2023-02-25 16:54:48 -0500

    update generate_data.py
    Added single SET generation functionality.

--- src/generate_data.py
+++ src/generate_data.py
@@ -5,26 +5,46 @@ import itertools
 import numpy as np
 
 name_combinations_items = [("3","4"),("S","D"),("N","Y"),("N","Y"),("A","D")]
+container_dict = {(0,0):["NNA","NYD","YND","YYD"],
+                    (0,1):["NND","NYA","YND","YYA"],
+                    (1,0):["NND","NYD","YNA","YYA"],
+                    (1,1):["NND","NYD","YND","YYA"]}
 
 def combinations(lst):
     # Generate all possible combinations of items in the tuples
     combos = [''.join(i) for i in list(itertools.product(*lst))]
     return combos
 
-set_combs = combinations(name_combinations_items)
-set_types = combinations(name_combinations_items[:2])
-set_criteria = combinations(name_combinations_items[2:4])
-print(set_criteria)
-
 def possible_strs(set_type):
     possible_values = [str(i) for i in range(1,1+int(set_type[0]))]
     str_length = int(set_type[0]) if set_type[1] == "S" else 2*int(set_type[0])-1
-    all_ints = [int(''.join(i)) for i in itertools.product(possible_values, repeat=str_length)]
+    all_ints = [''.join(i) for i in itertools.product(possible_values, repeat=str_length)]
     return all_ints
 
+def single_validate_str(string):
+    len_str = len(string)
+    set_rep = len(set(string))
+    all_same = 1 if set_rep == 1 else 0
+    all_diff = 1 if set_rep == len_str else 0
+    return container_dict[(all_same, all_diff)]
+
+def single_set_organization(set_type, set_dict, all_strs):
+    for i in all_strs:
+        container_mems = [set_type + j for j in single_validate_str(i)]
+        for k in container_mems:
+            set_dict[k].append(int(i))
+
+    return set_dict
 
 
 """ names = ['Player Name', 'Foo', 'Bar', -1, -1]
 scores = ['Score', 250, 500, 100, 30]
 np.savetxt('..\data\scores.csv', [p for p in zip(names, scores)], delimiter=',', fmt='%s')
 print([x for x in zip(names, scores)]) """
+
+if __name__ == "__main__":
+    set_types = combinations(name_combinations_items[:2])
+    set_dict = {i:[] for i in combinations(name_combinations_items)}
+    all_strs = possible_strs(set_types[2])
+    filled_set_dict = single_set_organization(set_types[2], set_dict, all_strs)
+    print(filled_set_dict)
